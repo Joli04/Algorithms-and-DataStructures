34375fea0fcae2c192885e7ed6f5f5f3fe3229f4
joliw <joliweibolt@gmail.com>
2022-11-08 18:08:22 +0100

    working on the weekends like usual

--- Assignment3/src/test/java/spotifycharts/EfficiencyTest.java
+++ Assignment3/src/test/java/spotifycharts/EfficiencyTest.java
@@ -18,28 +18,33 @@ public class EfficiencyTest {
     private Comparator<Song> rankingScheme = Song::compareByHighestStreamsCountTotal;
     private List<Song> heap;
     private Comparator<Song> heapComparator = Comparator.comparing(Song::getTitle);
+    private List<Song> hundred;
 
 
     @BeforeEach
-    public void setup(){
-        ChartsCalculator chartsCalculator = new ChartsCalculator(2L);
+    public void setup() {
+        ChartsCalculator chartsCalculator = new ChartsCalculator(1L);
         this.songSorter = new SongSorter();
         test = new SorterImpl<>();
-        fewSongs = new ArrayList<>(chartsCalculator.registerStreamedSongs(10));
-        manySongs = new ArrayList<>(chartsCalculator.registerStreamedSongs(250));
+        fewSongs = new ArrayList<>(chartsCalculator.registerStreamedSongs(100000));
+        hundred = new ArrayList<>(fewSongs);
+//        manySongs = new ArrayList<>(chartsCalculator.registerStreamedSongs(2));
     }
-    @Test
-    public void quicksortEfficiency(){
 
-//        100, 200, 400,
-//         800, 1600,
-        List<Song> hundred = new ArrayList<>(fewSongs);
-//        Collections.shuffle(hundred);
 
-        System.out.println(hundred);
+    public void quicksortEfficiency() {
         System.gc();
         test.quickSort(hundred, Comparator.comparing(Song::getStreamsCountTotal));
-        System.out.println(hundred);
+    }
+
+
+    @Test
+    public void test() {
+        long start = System.nanoTime();
+        quicksortEfficiency();
+        long end = System.nanoTime();
+        long duration = end - start;
+        System.out.println((double)duration*Math.pow(10,-9));
 
     }
 }
